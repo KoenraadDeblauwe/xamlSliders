a0ee8a597dd569d175fdc98bf3556a0a77836d8a
vincent tamboryn <vincent.tamboryn@hotmail.com>
2021-07-09 14:13:59 +0200

    Add project files.

---
+++ Test2/Test2/Views/RotationPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Test2.Models;
+using Test2.ViewModels;
+using Xamarin.Forms;
+
+namespace Test2.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
